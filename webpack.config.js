b940d156b25bae1d1dac138978477d292878a07a
stuffnting <richard@stuffnting.com>
2021-07-14 13:25:06 +0100

    Removed commented out source and destination

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,6 @@ const path = require("path");
 
 const CopyPlugin = require("copy-webpack-plugin");
 
-/*module.exports = {
-  ...defaultConfig,
-  entry: {
-    index: "./src/index.js",
-  },
-  output: {
-    path: path.join(__dirname, "/build"),
-    filename: "js/[name].js",
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: "php/*.php" },
-        { from: "plugin.php" },
-        { from: "css/*.css" },
-        { from: "acf-json/*.*" },
-      ],
-    }),
-  ],
-};*/
-
 module.exports = {
   ...defaultConfig,
   plugins: [
